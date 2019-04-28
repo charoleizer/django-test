da12fb305b833e61a0a3051edc8468ce0663e478
Victor Zanella <victor.t.y.z@hotmail.com>
2019-04-28 05:34:33 -0300

    incluido react no frontend

--- proj/produtos/urls.py
+++ proj/produtos/urls.py
@@ -1,9 +1,7 @@
 from rest_framework import routers
 from django.urls import path
-from .views import home
 from .api import ProdutoViewSet
 
 router = routers.DefaultRouter()
-router.register('api/produtos', ProdutoViewSet, 'produtos')
 
 urlpatterns = router.urls
--- proj/produtos/views.py
+++ proj/produtos/views.py
@@ -1,7 +1,3 @@
-from django.http import JsonResponse
 from django.shortcuts import render
-from .models import Produto
 
-def home(request):
-    produtos = Produto.objects.all()
-    return render(request, 'produtos.html', {'produtos': produtos})
+# Create your views here.
